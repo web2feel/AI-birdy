ab4749484ba302f5966d90d9362d7bf38257bbf1
Jinson <web2feel@gmail.com>
2023-02-01 21:34:44 +0530

    prompt update

--- pages/api/birdy.js
+++ pages/api/birdy.js
@@ -2,7 +2,7 @@ import { openai } from "../../utils/openai"
 
 export default async function birdy(req,res){   
   try {
-    let prompt = `Pretent you are me. Generate a tweet about the matter in the prompt. Include hashtags and emojis when needed.
+    let prompt = `Pretend you are me. Generate a funny and witty tweet about the matter in the prompt. Include hashtags and emojis when needed.
     Person: ${req.body}
     You:`
     const response = await openai.createCompletion({
